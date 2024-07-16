e34b83fdd15ff61c3b9ae5f6b7d1fbc8f9317b1b
Stephen <65339501+stephen-iamroot@users.noreply.github.com>
2024-07-17 00:29:07 +0100

    Delete Endpoint Scripts/Custom Fields/GeoLocation directory

--- Endpoint Scripts/Custom Fields/GeoLocation/GeoLocation.ps1
+++
@@ -1,3 +0,0 @@
-$result = Invoke-RestMethod -Uri ('http://ipinfo.io/'+(Invoke-WebRequest -UseBasicParsing -uri "http://ifconfig.me/ip").Content) | Select ip,hostname,city,region,country,loc,org,postal | ft -auto -HideTableHeaders | Out-String
-
-Ninja-Property-Set geoip $result
